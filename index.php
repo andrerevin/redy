43fab03b71997f07ee6cbd209ffeff8b0073e0a8
Andre Revin <revinson@ya.ru>
2017-04-10 09:53:29 +0300

    Blog Template
    2 flex col for blog feed

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+            <div class="d-flex flex-wrap">	
 
 		<?php
 		if ( have_posts() ) :
@@ -48,7 +49,8 @@ get_header(); ?>
 
 		endif; ?>
 
-		</main><!-- #main -->
+            </div>
+        </main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php

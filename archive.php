075859f3b547edb492a08755c69cb3ec02eaf0c1
Andre Revin <revinson@ya.ru>
2017-04-10 11:24:15 +0300

    404, search, archive
    404, search, archive

--- archive.php
+++ archive.php
@@ -11,7 +11,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+            <div class="d-flex flex-wrap">	
+                
 		<?php
 		if ( have_posts() ) : ?>
 
@@ -42,7 +43,8 @@ get_header(); ?>
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif; ?>
-
+                
+            </div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
